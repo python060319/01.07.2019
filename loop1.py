d3420ab44c742d4086e295f43c59ce29234c7a06
python060319 <51337287+python060319@users.noreply.github.com>
2019-07-01 18:12:57 +0300

    Create loop1.py

---
+++ loop1.py
@@ -0,0 +1,18 @@
+
+# iterator
+for n in range(100, 0, -1):
+    print(n)
+
+l1 = list(range(1, 200, 2))
+print(l1)
+
+# targil :
+# print kfulot 5 100- 500 (kolel)
+# print kfulot shel 10 1000-200 yored (kolel)
+# create list numbers odd between 155-300
+# [5, 8, 10, 200, -30, 50, 300, 2, 0] - print:
+#        index 2, 5, 8, ... without knowing len
+
+
+
+
